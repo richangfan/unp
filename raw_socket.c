50ba7fcd0ff52e7879909bcd7d696d16584f8610
richangfan <richangfan@outlook.com>
2021-04-04 18:55:09 +0800

    initialize

---
+++ raw_socket.c
@@ -0,0 +1,19 @@
+/**
+ * 原始套接字
+ */
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+    int sockfd;
+    sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if (sockfd < 0)
+    {
+        printf("创建原始套接字失败\n");
+        return 1;
+    }
+    return 0;
+}
