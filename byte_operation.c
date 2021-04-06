d59f5ff45c065760cd1c395963e4bc3863b66ed6
richangfan <richangfan@outlook.com>
2021-04-06 17:01:13 +0800

    字节操作函数

---
+++ byte_operation.c
@@ -0,0 +1,29 @@
+/**
+ * 字节操作函数
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void compare(int *i, int *j)
+{
+    if (memcmp(i, j, sizeof(*i)) == 0)
+        printf("i和j相等\n");
+    else
+        printf("i和j不等\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int i, j;
+    printf("i = %d, j = %d\n", i, j);
+    compare(&i, &j);
+    memset(&i, 0, sizeof(i));
+    printf("i = %d, j = %d\n", i, j);
+    compare(&i, &j);
+    memcpy(&j, &i, sizeof(i));
+    printf("i = %d, j = %d\n", i, j);
+    compare(&i, &j);
+    exit(0);
+}
